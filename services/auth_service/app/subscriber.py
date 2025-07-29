b68246e7f2f022394cb27f7496084fc79d069d7f
Vasiliy Burenko <pandafuky@gmail.com>
2025-07-29 17:37:21 +0300

    Redis Pub/Sub

---
+++ services/auth_service/app/subscriber.py
@@ -0,0 +1,16 @@
+import asyncio
+from app.cache import redis_client
+
+
+async def handle_message():
+    pubsub = redis_client.pubsub()
+    await pubsub.subscribe('notifications')
+    print("Подписан на канал 'notifications'...")
+
+    async for message in pubsub.listen():
+        if message['type'] == 'message':
+            print(f"Получено сообщение: {message['data']}")
+
+
+if __name__ == '__main__':
+    asyncio.run(handle_message())
